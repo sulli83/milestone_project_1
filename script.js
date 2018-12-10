c11f324ad5fb970a35e3f961d496f0ef29966abf
Shaun O'Sullivan <shaunosullivan83@gmail.com>
2018-12-10 21:33:20 +0000

    newest commit including start of readme

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
- $("#carousel").carousel()
- 
- $('.dropdown-toggle').click(function(){
+$('#dropdown-menu1').click(function(){
   $(this).toggleClass('.clicked');
 });
 
 
-(function(){
- var type= document.getElementById('services');
- var which_service ={
-  design:'Design',
-  construction:'Construction';
-  maintenance:'Maintenance
- };
- //when user changes the type select box
- addEvent(type,'change', function(){
-  if(this.value === 'choose'){
-   model.innerHTML = '<option>Please choose a type first</option';
-   return;
-   
-  }
- })
-})
